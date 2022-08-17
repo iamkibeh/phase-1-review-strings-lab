88f27bdde7e94e1936fe1361e5d1eb5808e5abf2
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-17 17:39:11 +0300

    Complete Asignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Kibet Immanuel';
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser+"!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+let firstChar = currentUser.charAt(0)
+console.log(firstChar);
+const shortGreeting = `Welcome, ${firstChar}!`
